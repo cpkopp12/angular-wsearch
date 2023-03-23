06fee7f128dcdaf387e377ccde06c3d77f52ff5f
cpkopp <cameronpkopp@gmail.com>
2023-03-22 19:35:05 -1000

    search term from sb componant to app

--- wsearch/src/app/app.component.ts
+++ wsearch/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wsearch';
+  onTerm(term: string) {
+    console.log("I am app and here is term ", term);
+  }
 }
