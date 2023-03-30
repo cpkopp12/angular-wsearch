523fe31e90a7b4db6142eac2c4bda4167b04413d
cpkopp <cameronpkopp@gmail.com>
2023-03-29 18:28:00 -1000

    hooked service to app

--- wsearch/src/app/app.component.ts
+++ wsearch/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { WikipediaService } from './wikipedia.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private wikipedia: WikipediaService) {
+
+  }
+
   onTerm(term: string) {
-    console.log("I am app and here is term ", term);
+    const results = this.wikipedia.search(term);
+    console.log(results);
   }
 }
